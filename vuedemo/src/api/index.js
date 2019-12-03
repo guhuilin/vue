d163f1766f1282c090a01da3e10635fcd704ab22
guhuilin <guhuilin_dream@163.com>
2019-12-03 18:35:18 +0800

    update api

--- vuedemo/src/api/index.js
+++ vuedemo/src/api/index.js
@@ -25,6 +25,25 @@ let getMine = () => {
   return get('/api/user/login-test')
 }
 
+// let detail = (id) => {
+//   return get('/api/article/details', {content_id: id})
+// }
+
+// // 我的药箱
+// let medicine = (params) => {
+//   return get('/api/medicine/list', params)
+// }
+
+// // 我的药箱详情
+// let medicinedetail = (id) => {
+//   return get('/api/medicine/details', {medicine_id: id})
+// }
+
+// // vip
+// let getMine = () => {
+//   return get('/api/user/login-test')
+// }
+
 let api = {
   list,
   detail,
