5876154d57e11321958f8877b3a9fc35cbaf5134
guhuilin <guhuilin_dream@163.com>
2019-12-16 17:57:31 +0800

    +

--- vueLeisure/src/components/App.vue
+++ vueLeisure/src/components/App.vue
@@ -16,7 +16,7 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class HelloWorld extends Vue {
+export default class App extends Vue {
   @Prop() private msg!: string;
 }
 </script>
--- vueLeisure/src/views/Home.vue
+++ vueLeisure/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <App msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import App from '@/components/App.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    App,
   },
 };
 </script>
