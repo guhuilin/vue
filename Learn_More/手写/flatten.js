3188567ef01cb571a1a81ad9cfcd501d8139ca34
guhuilin <guhuilin_dream@163.com>
2019-12-18 14:30:20 +0800

    flat

--- Learn_More/手写/flatten.js
+++ Learn_More/手写/flatten.js
@@ -7,3 +7,9 @@ const flatten = arr => {
 const res = flatten([[1, 2], 3, [[[4], 5]]]);
 
 console.log(res);
+
+// var arr1 = [1, 2, [3, 4]];
+// arr1.flat();
+// console.log(9**3)
+let obj = {a:1,b:[2,3],c:'string'}
+// console.log(obj.getOwnPropertyDescriptors())
