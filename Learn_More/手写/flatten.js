aeee95cf021521a3e6e5843ea595eec2457791a1
guhuilin <guhuilin_dream@163.com>
2019-12-21 14:06:26 +0800

    update flatten

--- Learn_More/手写/flatten.js
+++ Learn_More/手写/flatten.js
@@ -5,11 +5,19 @@ const flatten = arr => {
 };
 
 const res = flatten([[1, 2], 3, [[[4], 5]]]);
-
+console.log(...[[1, 2], 3, [[[4], 5]]]);
 console.log(res);
+
+const escapeChart = str => {
+    if(str.search('[') !== -1) str.replace('%5B','[')
+    if(str.search(']') !== -1) str.replace('%5D',']')
+    
+    return str
+}
+console.log(escapeChart("[12,3,45,'44']"),'str')
 
 // var arr1 = [1, 2, [3, 4]];
 // arr1.flat();
 // console.log(9**3)
-let obj = {a:1,b:[2,3],c:'string'}
+// let obj = {a:1,b:[2,3],c:'string'}
 // console.log(obj.getOwnPropertyDescriptors())
