3d6d90fa77da2463d02223815243ae3e67c96a7d
guhuilin <guhuilin_dream@163.com>
2020-05-09 11:02:21 +0800

    del blue_1129

--- blue_1129/src/components/flow/index.js
+++
@@ -1,9 +0,0 @@
-import SelectFlowComponent from './selectFlow.vue'
-
-const SelectFlow = {
-    install: (Vue) => {
-        Vue.component('SelectFlow', SelectFlowComponent)
-    }
-}
-
-export default SelectFlow
